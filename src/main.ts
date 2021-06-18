e867fedc12678cb4bd64612a13eadc07eadb1eb5
Lessley Dennington <lessleydennington@outlook.com>
2021-06-18 10:31:52 -0700

    Obtain version from manifest `PackageVersion` property
    Co-authored-by: Matthew John Cheetham <mjcheetham@github.com>

--- src/main.ts
+++ src/main.ts
@@ -184,6 +184,22 @@ async function run(): Promise<void> {
     core.debug('final manifest is:');
     core.debug(manifestText);
 
+core.debug(
+  "computing file path version from final manifest property 'PackageVersion'..."
+);
+const pkgVerRegEx = /PackageVersion:\s*(?<version>.*)/;
+const pkgVerMatch = manifestText.match(pkgVerRegEx);
+let pathVersion: string | undefined;
+if (pkgVerMatch?.groups?.version) {
+  pathVersion = pkgVerMatch.groups.version;
+} else {
+  core.warning(
+    "could not match 'PackageVersion' property in manifest; manifest may not be valid!"
+  );
+  pathVersion = version.toString();
+}
+core.debug(`path version is ${pathVersion}`);
+
     core.debug('computing manifest file path...');
     const manifestFilePath = `manifests/${id
       .charAt(0)
